22ec7e7fda698c217516395fa5209b65a12c93eb
Rafal Zawadzki <bluszcz@bluszcz.net>
2014-09-11 14:54:25 +0200

    Export's SQL

---
+++ ejabberd/jabberd2_export.sql
@@ -0,0 +1,4 @@
+COPY "authreg" TO '/tmp/jabberd2-export-authreg.csv' DELIMITER ',' CSV HEADER;
+COPY "vcard" TO '/tmp/jabberd2-export-vcard.csv' DELIMITER ',' CSV HEADER;
+COPY "roster-groups" TO '/tmp/jabberd2-export-roster-groups.csv' DELIMITER ',' CSV HEADER;
+COPY "roster-items" TO '/tmp/jabberd2-export-roster-items.csv' DELIMITER ',' CSV HEADER;
